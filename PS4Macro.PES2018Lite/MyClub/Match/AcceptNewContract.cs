d87db66ea4b1197c33cecb130a1a9dd386114617
leguims <leguims@free.fr>
2018-01-27 00:28:00 +0100

    upgrade AcceptNewContract.cs for:
     - update capture frame instead of sleeping between pad actions
     - clone RectMap extensionFocusHeader to 1-6 players and adjust Y-axis offset to match several players (up to 6).

--- PS4Macro.PES2018Lite/MyClub/Match/AcceptNewContract.cs
+++ PS4Macro.PES2018Lite/MyClub/Match/AcceptNewContract.cs
@@ -16,14 +16,105 @@ namespace PS4Macro.PES2018Lite.Match
             Hash = 68442442891264
         };
 
-        private static RectMap extensionFocusHeader = new RectMap()
+        private static RectMap extensionFocusHeader_1player = new RectMap()
         {
-            ID = "Match-AcceptNewContract-extensionHeader.png",
+            ID = "Match-AcceptNewContract-extensionHeader.png (Y offset = 1 player)",
+            X = 255,
+            Y = 241,
+            Width = 495,
+            Height = 90,
+            Hash = 28006088003583871
+        };
+
+        private static RectMap extensionFocusHeader_2players = new RectMap()
+        {
+            ID = "Match-AcceptNewContract-extensionHeader.png (Y offset = 2 players)",
             X = 255,
             Y = 229,
             Width = 495,
             Height = 90,
             Hash = 28006088003583871
+        };
+
+        private static RectMap extensionFocusHeader_3players = new RectMap()
+        {
+            ID = "Match-AcceptNewContract-extensionHeader.png (Y offset = 3 players)",
+            X = 255,
+            Y = 217,
+            Width = 495,
+            Height = 90,
+            Hash = 28006088003583871
+        };
+
+        private static RectMap extensionFocusHeader_4players = new RectMap()
+        {
+            ID = "Match-AcceptNewContract-extensionHeader.png (Y offset = 4 players)",
+            X = 255,
+            Y = 205,
+            Width = 495,
+            Height = 90,
+            Hash = 28006088003583871
+        };
+
+        private static RectMap extensionFocusHeader_5players = new RectMap()
+        {
+            ID = "Match-AcceptNewContract-extensionHeader.png (Y offset = 5 players)",
+            X = 255,
+            Y = 193,
+            Width = 495,
+            Height = 90,
+            Hash = 28006088003583871
+        };
+
+        private static RectMap extensionFocusHeader_6players = new RectMap()
+        {
+            ID = "Match-AcceptNewContract-extensionHeader.png (Y offset = 6 players)",
+            X = 255,
+            Y = 181,
+            Width = 495,
+            Height = 90,
+            Hash = 28006088003583871
+        };
+
+        private static RectMap extension2Focus = new RectMap()
+        {
+            ID = "Match-AcceptNewContract-extension2Focus.png",
+            X = 181,
+            Y = 464,
+            Width = 146,
+            Height = 19,
+            Hash = 9186919927859150719
+        };
+
+        private static RectMap extension3Focus = new RectMap()
+        {
+            ID = "Match-AcceptNewContract-extension3Focus.png",
+            X = 337,
+            Y = 377,
+            Width = 47,
+            Height = 23,
+            Hash = 9187131120130015615
+        };
+
+        private static RectMap extension4Focus = new RectMap()
+        {
+            ID = "Match-AcceptNewContract-extension4Focus.png",
+            X = 488,
+            Y = 366,
+            Width = 32,
+            Height = 21,
+            Hash = 16108853741370846942
+        };
+
+        private static RectMap extension5Focus = new RectMap()
+        {
+            // TODO : CAPTURE THE PICTURE !!
+            ID = "Match-AcceptNewContract-extension5Focus.png",
+            X = 1,
+            Y = 2,
+            Width = 1008,
+            Height = 729,
+            Hash = 68442442891264
         };
 
         private static RectMap extensionManager = new RectMap()
@@ -47,40 +138,70 @@ namespace PS4Macro.PES2018Lite.Match
         public override bool Match(ScriptBase script)
         {
             /* DEBUG */
-            Log.LogMatchTemplate(script, Name, new List<RectMap> { extension, extensionFocusHeader, extensionManager, extensionManagerFocusHeader });
+            Log.LogMatchTemplate(script, Name, new List<RectMap> { extension, extensionFocusHeader_1player, extensionFocusHeader_2players,
+                extensionFocusHeader_3players, extensionFocusHeader_4players, extensionFocusHeader_5players, extensionFocusHeader_6players,
+                extensionManager, extensionManagerFocusHeader });
 
-            return script.MatchTemplate(extension, 98) || script.MatchTemplate(extensionFocusHeader, 98)
-                || script.MatchTemplate(extensionManager, 98) || script.MatchTemplate(extensionManagerFocusHeader, 98);
+            return script.MatchTemplate(extension, 98) && 
+                ( script.MatchTemplate(extensionFocusHeader_1player, 98) || script.MatchTemplate(extensionFocusHeader_2players, 98) 
+                || script.MatchTemplate(extensionFocusHeader_3players, 98) || script.MatchTemplate(extensionFocusHeader_4players, 98) 
+                || script.MatchTemplate(extensionFocusHeader_5players, 98) || script.MatchTemplate(extensionFocusHeader_6players, 98) )
+                || script.MatchTemplate(extensionManager, 98) && script.MatchTemplate(extensionManagerFocusHeader, 98);
         }
 
         public override void OnMatched(ScriptBase script)
         {
+            bool manager = script.MatchTemplate(extensionManagerFocusHeader, 98);
+
+            Log.LogMessage(Name, "Start");
             // List of player to renew
             script.Press(new DualShockState() { DPad_Right = true });
             script.Press(new DualShockState() { Cross = true });
             Log.LogMessage(Name, "Accept to renew players/Manager");
+
             // Cost and kind of money
-            Thread.Sleep(5000);
+            while (!script.MatchTemplate(script.CaptureFrame(), extension2Focus, 98))
+            {
+                Thread.Sleep(1000);
+                Log.LogMessage(Name, "Waiting for 'Pay with GP money'");
+            }
             script.Press(new DualShockState() { Cross = true });
             Log.LogMessage(Name, "Pay with GP money");
+
             // Confirmation
-            Thread.Sleep(5000);
+            while (!script.MatchTemplate(script.CaptureFrame(), extension3Focus, 98))
+            {
+                Thread.Sleep(1000);
+                Log.LogMessage(Name, "Waiting for 'Confirm'");
+            }
             script.Press(new DualShockState() { DPad_Right = true });
             script.Press(new DualShockState() { Cross = true });
             Log.LogMessage(Name, "Confirm");
+
             // Transaction'status
-            Thread.Sleep(5000);
+            while (!script.MatchTemplate(script.CaptureFrame(), extension4Focus, 98))
+            {
+                Thread.Sleep(1000);
+                Log.LogMessage(Name, "Waiting for 'Skip transaction status'");
+            }
             script.Press(new DualShockState() { Cross = true });
             Log.LogMessage(Name, "Skip transaction'status");
 
             /* Only for manager */
-            if (script.MatchTemplate(extensionManagerFocusHeader, 98))
+            if (manager)
             {
                 // Transaction'status 2
+                /*while (!script.MatchTemplate(script.CaptureFrame(), extension5Focus, 98))
+                {
+                    Thread.Sleep(1000);
+                    Log.LogMessage(Name, "Waiting for 'Skip transaction status2'");
+                }*/
+                // TODO : CAPTURE THE PICTURE !!
                 Thread.Sleep(5000);
                 script.Press(new DualShockState() { Cross = true });
-                Log.LogMessage(Name, "Skip transaction'status 2");
+                Log.LogMessage(Name, "Skip transaction'status2");
             }
+            Log.LogMessage(Name, "End");
         }
     }
 }
